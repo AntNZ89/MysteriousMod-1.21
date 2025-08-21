ce20a59b2a34c32fa7e5689c777cc0df3ff9faf3
antND <antonlenz508@gmail.com>
2025-08-21 21:15:32 +0200

    Update

--- src/main/java/net/antnz/mysteriousmod/item/custom/MysteriousAxeItem.java
+++ src/main/java/net/antnz/mysteriousmod/item/custom/MysteriousAxeItem.java
@@ -1,22 +1,113 @@
 package net.antnz.mysteriousmod.item.custom;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableMap;
+import net.antnz.mysteriousmod.block.ModBlocks;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.block.*;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.item.AxeItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Optional;
 
 public class MysteriousAxeItem extends AxeItem {
+
+//    List<Block> blockList = List.of(Blocks.WO);
+
     public MysteriousAxeItem(ToolMaterial toolMaterial, Settings settings) {
         super(toolMaterial, settings);
         settings.rarity(Rarity.EPIC);
     }
 
-    @Override
-    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        target.addStatusEffect(new StatusEffectInstance(StatusEffects.WIND_CHARGED, 1200, 3));
-        return super.postHit(stack, target, attacker);
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        PlayerEntity playerEntity = context.getPlayer();
+        Block block = world.getBlockState(blockPos).getBlock();
+
+        if (world instanceof ServerWorld){
+            if (!(block instanceof PillarBlock)){
+
+                world.setBlockState(blockPos, ModBlocks.MYSTERIOUS_AXE_BLOCK.getDefaultState());
+                world.playSound(null, blockPos, SoundEvents.BLOCK_LEVER_CLICK, SoundCategory.BLOCKS);
+                return ActionResult.SUCCESS;
+
+            }
+        }
+
+
+        if (shouldCancelStripAttempt(context)) {
+            return ActionResult.PASS;
+        } else {
+            Optional<BlockState> optional = this.tryStrip(world, blockPos, playerEntity, world.getBlockState(blockPos));
+            if (optional.isEmpty()) {
+                return ActionResult.PASS;
+            } else {
+                ItemStack itemStack = context.getStack();
+                if (playerEntity instanceof ServerPlayerEntity) {
+                    Criteria.ITEM_USED_ON_BLOCK.trigger((ServerPlayerEntity)playerEntity, blockPos, itemStack);
+                }
+
+                world.setBlockState(blockPos, (BlockState)optional.get(), 11);
+                world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, (BlockState)optional.get()));
+                if (playerEntity != null) {
+                    itemStack.damage(1, playerEntity, LivingEntity.getSlotForHand(context.getHand()));
+                }
+
+                return ActionResult.success(world.isClient);
+            }
+        }
     }
+
+    private static boolean shouldCancelStripAttempt(ItemUsageContext context) {
+        PlayerEntity playerEntity = context.getPlayer();
+        return context.getHand().equals(Hand.MAIN_HAND) && playerEntity.getOffHandStack().isOf(Items.SHIELD) && !playerEntity.shouldCancelInteraction();
+    }
+
+    private Optional<BlockState> tryStrip(World world, BlockPos pos, @Nullable PlayerEntity player, BlockState state) {
+        Optional<BlockState> optional = this.getStrippedState(state);
+        if (optional.isPresent()) {
+            world.playSound(player, pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            return optional;
+        } else {
+            Optional<BlockState> optional2 = Oxidizable.getDecreasedOxidationState(state);
+            if (optional2.isPresent()) {
+                world.playSound(player, pos, SoundEvents.ITEM_AXE_SCRAPE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                world.syncWorldEvent(player, 3005, pos, 0);
+                return optional2;
+            } else {
+                Optional<BlockState> optional3 = Optional.ofNullable((Block)((BiMap)HoneycombItem.WAXED_TO_UNWAXED_BLOCKS.get()).get(state.getBlock())).map((block) -> block.getStateWithProperties(state));
+                if (optional3.isPresent()) {
+                    world.playSound(player, pos, SoundEvents.ITEM_AXE_WAX_OFF, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    world.syncWorldEvent(player, 3004, pos, 0);
+                    return optional3;
+                } else {
+                    return Optional.empty();
+                }
+            }
+        }
+    }
+
+    private Optional<BlockState> getStrippedState(BlockState state) {
+        return Optional.ofNullable((Block)STRIPPED_BLOCKS.get(state.getBlock())).map((block) -> (BlockState)block.getDefaultState().with(PillarBlock.AXIS, (Direction.Axis)state.get(PillarBlock.AXIS)));
+    }
+
+
+
 }
