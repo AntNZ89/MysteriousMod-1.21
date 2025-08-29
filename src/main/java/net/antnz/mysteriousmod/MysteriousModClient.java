31ee343f054cd5bb3f7cd3ed714cad59e811fdce
antND <antonlenz508@gmail.com>
2025-08-29 21:24:19 +0200

    Update

--- src/main/java/net/antnz/mysteriousmod/MysteriousModClient.java
+++ src/main/java/net/antnz/mysteriousmod/MysteriousModClient.java
@@ -9,8 +9,9 @@ public class MysteriousModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
+        MysteriousMod.LOGGER.info("------------------- WORKING -------------- WORKING -----------------");
+
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_TRAPDOOR, RenderLayer.getCutout());
-
     }
 }
